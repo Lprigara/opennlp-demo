5e08cc728ecf01e8cbb40f9650849637cf587a4c
lprigara <lprigara@gmail.com>
2016-05-31 22:05:18 +0100

    Recibir ficheros de texto en inglés, procesar y obtener los tokens

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -16,33 +16,31 @@ public class TokenizerMain
 	public static void main( String[] args ) throws Exception
 	{
 		
-		// the provided model
-		// InputStream modelIn = new FileInputStream( "models/en-token.bin" );
-		BufferedReader br = new BufferedReader(new FileReader("prueba.txt"));
-		try {
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
+		public String readFile(String file){ 
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+			    StringBuilder sb = new StringBuilder();
+		   	    String line = br.readLine();
 
-		    List<String> wordList = new ArrayList<String>();
+		   	     List<String> wordList = new ArrayList<String>();
 		    
-		    while (line != null) {
-		    	StringTokenizer stringTokenizer = new StringTokenizer(line);
-	                while(stringTokenizer.hasMoreTokens()){
-                		String word= stringTokenizer.nextToken();
-                		if(!wordList.contains(word)){
-                			wordList.add(word);
-                		}	
-		            	line = br.readLine();
-		        }
-		    }
-		    
-		    for (String word : wordList) {
-			System.out.println("Word: " + word);
-		    }
-		}catch( RuntimeException e){
-			System.out.println("Al leer el fichero se ha producido la siguiente excepción" + e.getMessage());
-		} finally {
-		    br.close();
+		    	     while (line != null) {
+		    	     	StringTokenizer stringTokenizer = new StringTokenizer(line);
+	                	while(stringTokenizer.hasMoreTokens()){
+                			String word= stringTokenizer.nextToken();
+                			if(!wordList.contains(word)){
+                				wordList.add(word);
+                			}	
+		            		line = br.readLine();
+		        	}
+		    	     }
+			}catch( RuntimeException e){
+				System.out.println("Al leer el fichero se ha producido la siguiente excepción" + e.getMessage());
+			} finally {
+			    br.close();
+			    return wordList;
+			}
+			return wordList;
 		}
 		
 		// the model we trained
@@ -55,10 +53,7 @@ public class TokenizerMain
 			Tokenizer tokenizer = new TokenizerME(model);
 			
 				/* note what happens with the "three depending on which model you use */
-			String[] tokens = tokenizer.tokenize
-					(  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, " 
-							+ " mentions \"three Mounts raised by the Indians over three of their Great Kings" 
-							+ " who were killed in the Wars.\"" );
+			String[] tokens = tokenizer.tokenize(readFile("prueba.txt"));
 			
 			for( String token : tokens )
 			{
